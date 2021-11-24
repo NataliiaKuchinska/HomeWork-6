c940f4ada6cba6406832d11b528308b447345f82
N K <n.kuchinska@gmail.com>
2021-11-24 19:35:16 +0200

    Add project files.

---
+++ HomeWork-6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TwoDimentionalArrayLib;
+
+namespace HomeWork_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4, 4];
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(1, 20);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(TDATaskLib.GetMinElement(array));
+            Console.WriteLine(TDATaskLib.GetMaxElement(array));
+            Console.WriteLine(TDATaskLib.GetMinElementIndex(array));
+            Console.WriteLine(TDATaskLib.GetMaxElementIndex(array));
+            Console.WriteLine(TDATaskLib.NumMaxinNeighbourElements(array));
+            TDATaskLib.MainDiagonalSwap(array);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ TwoDimentionalArrayLib/TDATaskLib.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace TwoDimentionalArrayLib
+{
+    public class TDATaskLib
+    {
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static (int, int) GetMinElementIndex(int[,] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+            else
+            {
+                int ind1 = 0;
+                int ind2 = 0;
+                int min = array[0, 0];
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (min > array[i, j])
+                        {
+                            min = array[i, j];
+                            ind1 = i;
+                            ind2 = j;
+                        }
+                    }
+                }
+
+                return (ind1, ind2);
+            }
+        }
+
+        public static (int, int) GetMaxElementIndex(int[,] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+            else
+            {
+                int ind1 = 0;
+                int ind2 = 0;
+                int max = array[0, 0];
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (max < array[i, j])
+                        {
+                            max = array[i, j];
+                            ind1 = i;
+                            ind2 = j;
+                        }
+                    }
+                }
+
+                return (ind1, ind2);
+            }
+        }
+
+        public static int GetMinElement(int[,] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+            else
+            {
+                var Index = GetMinElementIndex(array);
+                return array[Index.Item1, Index.Item2];
+            }
+        }
+        public static int GetMaxElement(int[,] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+            else
+            {
+                var Index = GetMaxElementIndex(array);
+                return array[Index.Item1, Index.Item2];
+            }
+        }
+
+        public static void MainDiagonalSwap(int[,] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+            else
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = i + 1; j < array.GetLength(1); j++)
+                    {
+                        Swap(ref array[i, j], ref array[j, i]);
+                    }
+                }
+            }
+        }
+        public static int NumMaxinNeighbourElements(int[,] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+            else
+            {
+                int result = 0;
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        bool ind = true;
+                        if (i - 1 >= 0)
+                        {
+                            ind = ind && (array[i, j] > array[i - 1, j]);
+                        }
+                        if (i + 1 < array.GetLength(0))
+                        {
+                            ind = ind && (array[i, j] > array[i + 1, j]);
+                        }
+                        if (j - 1 >= 0)
+                        {
+                            ind = ind && (array[i, j] > array[i, j - 1]);
+                        }
+                        if (j + 1 < array.GetLength(1))
+                        {
+                            ind = ind && (array[i, j] > array[i, j + 1]);
+                        }
+                        if (ind == true)
+                        {
+                            ++result;
+                        }
+
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
